a27218bfc7576051b336d6a8b204a6ca3315cfbe
yakelixir <yakelixir@gmail.com>
2019-06-26 20:51:12 -0500

    additional notes changes to the display of page

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ from routes.search import search_blueprint
 app = Flask(__name__)
 app.register_blueprint(search_blueprint)
 
+
 if __name__ == "__main__":
-    app.run("0.0.0.0",port=8005,debug=False,threaded=True)
+    app.run("0.0.0.0",port=8005,debug=True,threaded=True)
